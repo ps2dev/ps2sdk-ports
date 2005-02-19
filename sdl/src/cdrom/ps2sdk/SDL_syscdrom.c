f33abeba229f21396c742d1a4c267c5d8e359473
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-02-19 14:13:07 +0000

    added cdrom stubs

---
+++ sdl/src/cdrom/ps2sdk/SDL_syscdrom.c
@@ -0,0 +1,141 @@
+/*
+    SDL - Simple DirectMedia Layer
+    Copyright (C) 1997-2004 Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Gil Megidish
+    gil@megidish.net
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
+
+#ifdef SAVE_RCSID
+static char rcsid =
+ "@(#) $Id$";
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "SDL_error.h"
+#include "SDL_cdrom.h"
+#include "SDL_syscdrom.h"
+
+#include <libcdvd.h>
+
+static const char *SDL_SYS_CDName(int drive)
+{
+	return "cdrom0:";
+}
+
+static int SDL_SYS_CDOpen(int drive)
+{
+	return(drive);
+}
+
+static int SDL_SYS_CDGetTOC(SDL_CD *cdrom)
+{
+	return -1;
+}
+
+/* Get CD-ROM status */
+static CDstatus SDL_SYS_CDStatus(SDL_CD *cdrom, int *position)
+{
+	s32 status;
+
+	return CD_ERROR;
+
+	status = cdStatus();
+	switch(status)
+	{
+		case CDVD_STAT_STOP:
+		return CD_STOPPED;
+
+		case CDVD_STAT_SEEK:
+		case CDVD_STAT_SPIN:
+		case CDVD_STAT_READ:
+		return CD_PLAYING;
+
+		case CDVD_STAT_PAUSE:
+		return CD_PAUSED;
+
+		case CDVD_STAT_OPEN:
+		return CD_TRAYEMPTY;
+
+		default:
+		return CD_ERROR;
+	}
+}
+
+static int SDL_SYS_CDPlay(SDL_CD *cdrom, int start, int length)
+{
+	return 0;
+}
+
+static int SDL_SYS_CDPause(SDL_CD *cdrom)
+{
+	return 0;
+}
+
+/* Resume play */
+static int SDL_SYS_CDResume(SDL_CD *cdrom)
+{
+	return 0;
+}
+
+static int SDL_SYS_CDStop(SDL_CD *cdrom)
+{
+	return 0;
+}
+
+static int SDL_SYS_CDEject(SDL_CD *cdrom)
+{
+	/* not supported by ps2 */
+	return -1;
+}
+
+static void SDL_SYS_CDClose(SDL_CD *cdrom)
+{
+}
+
+void SDL_SYS_CDQuit(void)
+{
+}
+
+int SDL_SYS_CDInit(void)
+{
+	/* driver capabilities */
+	SDL_CDcaps.Name = SDL_SYS_CDName;
+	SDL_CDcaps.Open = SDL_SYS_CDOpen;
+	SDL_CDcaps.GetTOC = SDL_SYS_CDGetTOC;
+	SDL_CDcaps.Status = SDL_SYS_CDStatus;
+	SDL_CDcaps.Play = SDL_SYS_CDPlay;
+	SDL_CDcaps.Pause = SDL_SYS_CDPause;
+	SDL_CDcaps.Resume = SDL_SYS_CDResume;
+	SDL_CDcaps.Stop = SDL_SYS_CDStop;
+	SDL_CDcaps.Eject = SDL_SYS_CDEject;
+	SDL_CDcaps.Close = SDL_SYS_CDClose;
+
+	SDL_numcds = 1;
+
+	return 0;
+}
+
+/*
+   all of these functions are merely stubs. I lost my code in a bsod
+                                                             -- gawd
+*/
