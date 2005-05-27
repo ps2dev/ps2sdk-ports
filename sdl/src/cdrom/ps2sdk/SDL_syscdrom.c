3cbe20d505c17a07543b1a3cbb3360953c5b4e12
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-05-27 17:07:42 +0000

    premature CDROM support

--- sdl/src/cdrom/ps2sdk/SDL_syscdrom.c
+++ sdl/src/cdrom/ps2sdk/SDL_syscdrom.c
@@ -28,18 +28,21 @@ static char rcsid =
  "@(#) $Id$";
 #endif
 
+/* Functions for system-level CD-ROM audio control */
+
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <SDL.h>
 #include "SDL_error.h"
-#include "SDL_cdrom.h"
+#include <SDL_cdrom.h>
 #include "SDL_syscdrom.h"
 
-#include <libcdvd.h>
+#include <audsrv.h>
 
 static const char *SDL_SYS_CDName(int drive)
 {
-	return "cdrom0:";
+	return "cdrom:";
 }
 
 static int SDL_SYS_CDOpen(int drive)
@@ -49,65 +52,85 @@ static int SDL_SYS_CDOpen(int drive)
 
 static int SDL_SYS_CDGetTOC(SDL_CD *cdrom)
 {
-	return -1;
+	int i;
+
+	cdrom->numtracks = audsrv_get_numtracks();
+	for (i=0; i<cdrom->numtracks; i++)
+	{
+		cdrom->track[i].id = i;
+		cdrom->track[i].type = SDL_AUDIO_TRACK; // FIXME;
+		cdrom->track[i].offset = audsrv_get_track_offset(i);
+		cdrom->track[i].length = audsrv_get_track_offset(i+1) - cdrom->track[i].offset;
+	}
+
+	return 0;
 }
 
-/* Get CD-ROM status */
 static CDstatus SDL_SYS_CDStatus(SDL_CD *cdrom, int *position)
 {
-	s32 status;
+	int status = audsrv_get_cd_status();
 
-	return CD_ERROR;
-
-	status = cdStatus();
 	switch(status)
 	{
-		case CDVD_STAT_STOP:
+		case 0x00:
+		/* stopped */
 		return CD_STOPPED;
 
-		case CDVD_STAT_SEEK:
-		case CDVD_STAT_SPIN:
-		case CDVD_STAT_READ:
-		return CD_PLAYING;
-
-		case CDVD_STAT_PAUSE:
-		return CD_PAUSED;
-
-		case CDVD_STAT_OPEN:
+		case 0x01:
+		/* tray open */
 		return CD_TRAYEMPTY;
 
+		case 0x02:
+		case 0x06:
+		case 0x12:
+		/* spinning, reading, or seeking */
+		return CD_PLAYING;
+
+		case 0x0a:
+		/* paused */
+		return CD_PAUSED;
+
+		case 0x20:
 		default:
+		/* error or unknown */
 		return CD_ERROR;
 	}
 }
 
+/* Start play */
 static int SDL_SYS_CDPlay(SDL_CD *cdrom, int start, int length)
 {
-	return 0;
+	audsrv_play_sectors(start, start + length);
 }
 
+/* Pause play */
 static int SDL_SYS_CDPause(SDL_CD *cdrom)
 {
+	audsrv_pause_cd();
 	return 0;
 }
 
 /* Resume play */
 static int SDL_SYS_CDResume(SDL_CD *cdrom)
 {
+	audsrv_resume_cd();
 	return 0;
 }
 
+/* Stop play */
 static int SDL_SYS_CDStop(SDL_CD *cdrom)
 {
+	audsrv_stop_cd();
 	return 0;
 }
 
+/* Eject the CD-ROM */
 static int SDL_SYS_CDEject(SDL_CD *cdrom)
 {
-	/* not supported by ps2 */
 	return -1;
 }
 
+/* Close the CD-ROM handle */
 static void SDL_SYS_CDClose(SDL_CD *cdrom)
 {
 }
@@ -118,7 +141,19 @@ void SDL_SYS_CDQuit(void)
 
 int SDL_SYS_CDInit(void)
 {
-	/* driver capabilities */
+	printf("SDL CDROM support is a work in progress;\nPlease do not use it at the moment\n");
+	SDL_OutOfMemory();
+ 	return -1;
+
+	/* must have audsrv initialized and ready to go */
+	if (SDL_WasInit(SDL_INIT_AUDIO) == 0)
+	{
+		SDL_SetError("SDL_INIT_AUDIO must be enabled");
+		return -1;
+	}
+
+	/* Fill in our driver capabilities */
+	SDL_numcds = 1;
 	SDL_CDcaps.Name = SDL_SYS_CDName;
 	SDL_CDcaps.Open = SDL_SYS_CDOpen;
 	SDL_CDcaps.GetTOC = SDL_SYS_CDGetTOC;
@@ -129,13 +164,5 @@ int SDL_SYS_CDInit(void)
 	SDL_CDcaps.Stop = SDL_SYS_CDStop;
 	SDL_CDcaps.Eject = SDL_SYS_CDEject;
 	SDL_CDcaps.Close = SDL_SYS_CDClose;
-
-	SDL_numcds = 1;
-
 	return 0;
 }
-
-/*
-   all of these functions are merely stubs. I lost my code in a bsod
-                                                             -- gawd
-*/
