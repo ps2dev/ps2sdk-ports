629d57fac3c4c4a749819dfa10f574cad0bb1ba0
gawd <gawd@fd1d1198-7684-4768-b9be-5b3c44f97897>
2005-06-02 21:44:24 +0000

    SDL_Delay test

--- sdl/test/testtimer.c
+++ sdl/test/testtimer.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-	int now_ms, before_sec;
+	int now_ms, before_ms;
+	int now_sec, before_sec;
 
 	if (SDL_Init(SDL_INIT_TIMER) < 0)
 	{
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 	now_ms = SDL_GetTicks();
 	while (now_ms < 60*1000)
 	{
-		int now_sec = now_ms / 1000;
+		now_sec = now_ms / 1000;
 		if (now_sec != before_sec)
 		{
 			printf("Time: %02d:%02d\n", now_sec / 60, now_sec % 60);
@@ -27,6 +28,13 @@ int main(int argc, char *argv[])
 
 		now_ms = SDL_GetTicks();
 	}
+
+	/* test 2: test SDL_Delay */
+	printf("test 2: sleeping for 30 seconds with SDL_Delay\n");
+	before_ms = SDL_GetTicks();
+	SDL_Delay(30*1000);
+	now_ms = SDL_GetTicks();
+	printf("Ticks differ %d (should be 30000)\n", now_ms - before_ms);
 
 	printf("test ended\n");
 	return 0;
