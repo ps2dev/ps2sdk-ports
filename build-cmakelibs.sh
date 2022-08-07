74fd3c82cd93fc108d344ff2ea0de81696ab36b6
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-08-07 17:18:57 +0200

    Add libmodplug

--- build-cmakelibs.sh
+++ build-cmakelibs.sh
@@ -57,6 +57,10 @@ git clone https://github.com/xiph/opus.git || { exit 1; }
 # till they release a new version with cmake compatibility
 git clone https://github.com/xiph/opusfile.git || { exit 1; } 
 (cd opusfile && git checkout cf218fb54929a1f54e30e2cb208a22d08b08c889 && cd -) || { exit 1; }
+# We need to clone the whole repo and point to the specific hash for now, 
+# till they release a new version with cmake compatibility
+git clone https://github.com/Konstanty/libmodplug.git || { exit 1; } 
+(cd libmodplug && git checkout d1b97ed0020bc620a059d3675d1854b40bd2608d && cd -) || { exit 1; }
 
 ##
 ## Build cmake projects
@@ -71,4 +75,5 @@ build vorbis
 build libxmp
 build opus
 build opusfile "-DOP_DISABLE_HTTP=ON -DOP_DISABLE_DOCS=ON -DOP_DISABLE_EXAMPLES=ON"
+build libmodplug
 cd ..
