374999f7540b42255cbc41ceb5b8f621ad5ff942
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-12-27 17:48:54 +0100

    Fix installation of gskit

--- build-cmakelibs.sh
+++ build-cmakelibs.sh
@@ -106,6 +106,7 @@ build mikmod-mikmod/libmikmod -DENABLE_SHARED=0
 build jsoncpp -DBUILD_OBJECT_LIBS=OFF -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF
 
 # gsKit is mandatory for SDL, gsKit doesn't have cmake configuration yet, however we are going to put it here to solve cycling dependencies
+make -C gsKit -j "$PROC_NR"
 make -C gsKit -j "$PROC_NR" install
 
 build SDL -DCMAKE_POSITION_INDEPENDENT_CODE=OFF -DSDL_TESTS=OFF
